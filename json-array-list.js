77c403a0b750398e650993c7d6531f6473a90dd2
lihen12 <henryli0@yahoo.com>
2018-10-19 14:18:31 -0700

    Changed file name

---
+++ json-array-list.js
@@ -0,0 +1,58 @@
+var workoutBodyType = [
+  body1:{name:"legs"},
+  body2:{name:"back"},
+  body3:{name:"arms"},
+  body4:{name:"chest"},
+  body5:{name:"shoulders"}
+]
+
+var legsType = [
+  legs1:{name:"squats",weight:"",reps:""},
+  legs2:{name:"leg-press",weight:"",reps:""},
+  legs3:{name:"lunges",weight:"",reps:""},
+  legs4:{name:"hamstring-deadlift",weight:"",reps:""},
+  legs5:{name:"calf-raise",weight:"",reps:""},
+  legs6:{name:"step-up",weight:"",reps:""}
+]
+
+var backType = [
+  back1:{name:"deadlift",weight:"",reps:""},
+  back2:{name:"pulldown",weight:"",reps:""},
+  back3:{name:"seated-row",weight:"",reps:""},
+  back4:{name:"bent-over-row",weight:"",reps:""},
+  back5:{name:"single-dumbell-row",weight:"",reps:""}
+]
+
+var armsType = [
+  arms1:{name:"dumbell-curl",weight:"",reps:""},
+  arms2:{name:"hammer-curl",weight:"",reps:""},
+  arms3:{name:"seated-hammer-curl",weight:"",reps:""},
+  arms4:{name:"seated-dumbell-curl",weight:"",reps:""},
+  arms5:{name:"barbell-curl",weight:"",reps:""},
+  arms6:{name:"cable-curl",weight:"",reps:""},
+  arms7:{name:"tricep-extension",weight:"",reps:""},
+  arms8:{name:"lying-tricep-extension",weight:"",reps:""},
+  arms9:{name:"cable-tricep-extension",weight:"",reps:""}
+]
+
+var chestType = [
+  chest1:{name:"chest-press",weight:"",reps:""},
+  chest2:{name:"dumbell-chest-press",weight:"",reps:""},
+  chest3:{name:"incline-chest-press",weight:"",reps:""},
+  chest4:{name:"dumbell-incline-chest-press",weight:"",reps:""},
+  chest5:{name:"decline-press",weight:"",reps:""},
+  chest6:{name:"dumbell-decline-chest-press",weight:"",reps:""},
+  chest7:{name:"cable-fly",weight:"",reps:""},
+  chest8:{name:"cable-incline-fly",weight:"",reps:""},
+  chest9:{name:"cable-decline-fly",weight:"",reps:""}
+]
+
+var shouldersType = [
+  shoulders1:{name:"dumbell-shoulder-press",weight:"",reps:""},
+  shoulders2:{name:"barbell-shoulder-press",weight:"",reps:""},
+  shoulders3:{name:"dumbell-shoulder-fly",weight:"",reps:""},
+  shoulders4:{name:"face-pull",weight:"",reps:""},
+  shoulders5:{name:"armold-press",weight:"",reps:""}
+]
+
+
