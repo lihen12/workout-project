ad5a9ac1306044e57f531a730644e82175f4408e
lihen12 <henryli0@yahoo.com>
2018-10-19 14:16:51 -0700

    Added more objects to json array

--- json.js
+++ json.js
@@ -7,50 +7,52 @@ var workoutBodyType = [
 ]
 
 var legsType = [
-  legs1:{name:"squats"},
-  legs2:{name:"leg-press"},
-  legs3:{name:"lunges"},
-  legs4:{name:"hamstring-deadlift"},
-  legs5:{name:"calf-raise"},
-  legs6:{name:"step-up"}
+  legs1:{name:"squats",weight:"",reps:""},
+  legs2:{name:"leg-press",weight:"",reps:""},
+  legs3:{name:"lunges",weight:"",reps:""},
+  legs4:{name:"hamstring-deadlift",weight:"",reps:""},
+  legs5:{name:"calf-raise",weight:"",reps:""},
+  legs6:{name:"step-up",weight:"",reps:""}
 ]
 
 var backType = [
-  back1:{name:"deadlift"},
-  back2:{name:"pulldown"},
-  back3:{name:"seated-row"},
-  back4:{name:"bent-over-row"},
-  back5:{name:"single-dumbell-row"}
+  back1:{name:"deadlift",weight:"",reps:""},
+  back2:{name:"pulldown",weight:"",reps:""},
+  back3:{name:"seated-row",weight:"",reps:""},
+  back4:{name:"bent-over-row",weight:"",reps:""},
+  back5:{name:"single-dumbell-row",weight:"",reps:""}
 ]
 
 var armsType = [
-  arms1:{name:"dumbell-curl"},
-  arms2:{name:"hammer-curl"},
-  arms3:{name:"seated-hammer-curl"},
-  arms4:{name:"seated-dumbell-curl"},
-  arms5:{name:"barbell-curl"},
-  arms6:{name:"cable-curl"},
-  arms7:{name:"tricep-extension"},
-  arms8:{name:"lying-tricep-extension"},
-  arms9:{name:"cable-tricep-extension"}
+  arms1:{name:"dumbell-curl",weight:"",reps:""},
+  arms2:{name:"hammer-curl",weight:"",reps:""},
+  arms3:{name:"seated-hammer-curl",weight:"",reps:""},
+  arms4:{name:"seated-dumbell-curl",weight:"",reps:""},
+  arms5:{name:"barbell-curl",weight:"",reps:""},
+  arms6:{name:"cable-curl",weight:"",reps:""},
+  arms7:{name:"tricep-extension",weight:"",reps:""},
+  arms8:{name:"lying-tricep-extension",weight:"",reps:""},
+  arms9:{name:"cable-tricep-extension",weight:"",reps:""}
 ]
 
 var chestType = [
-  chest1:{name:"chest-press"},
-  chest2:{name:"dumbell-chest-press"},
-  chest3:{name:"incline-chest-press"},
-  chest4:{name:"dumbell-incline-chest-press"},
-  chest5:{name:"decline-press"},
-  chest6:{name:"dumbell-decline-chest-press"},
-  chest7:{name:"cable-fly"},
-  chest8:{name:"cable-incline-fly"},
-  chest9:{name:"cable-decline-fly"}
+  chest1:{name:"chest-press",weight:"",reps:""},
+  chest2:{name:"dumbell-chest-press",weight:"",reps:""},
+  chest3:{name:"incline-chest-press",weight:"",reps:""},
+  chest4:{name:"dumbell-incline-chest-press",weight:"",reps:""},
+  chest5:{name:"decline-press",weight:"",reps:""},
+  chest6:{name:"dumbell-decline-chest-press",weight:"",reps:""},
+  chest7:{name:"cable-fly",weight:"",reps:""},
+  chest8:{name:"cable-incline-fly",weight:"",reps:""},
+  chest9:{name:"cable-decline-fly",weight:"",reps:""}
 ]
 
 var shouldersType = [
-  shoulders1:{name:"dumbell-shoulder-press"},
-  shoulders2:{name:"barbell-shoulder-press"},
-  shoulders3:{name:"dumbell-shoulder-fly"},
-  shoulders4:{name:"face-pull"},
-  shoulders5:{name:"armold-press"}
+  shoulders1:{name:"dumbell-shoulder-press",weight:"",reps:""},
+  shoulders2:{name:"barbell-shoulder-press",weight:"",reps:""},
+  shoulders3:{name:"dumbell-shoulder-fly",weight:"",reps:""},
+  shoulders4:{name:"face-pull",weight:"",reps:""},
+  shoulders5:{name:"armold-press",weight:"",reps:""}
 ]
+
+
