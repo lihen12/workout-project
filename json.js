2c73725948530c5c20378c3587f223ed3b6b1a19
lihen12 <henryli0@yahoo.com>
2018-10-19 14:12:06 -0700

    created tentative list with workout type and body

---
+++ json.js
@@ -0,0 +1,56 @@
+var workoutBodyType = [
+  body1:{name:"legs"},
+  body2:{name:"back"},
+  body3:{name:"arms"},
+  body4:{name:"chest"},
+  body5:{name:"shoulders"}
+]
+
+var legsType = [
+  legs1:{name:"squats"},
+  legs2:{name:"leg-press"},
+  legs3:{name:"lunges"},
+  legs4:{name:"hamstring-deadlift"},
+  legs5:{name:"calf-raise"},
+  legs6:{name:"step-up"}
+]
+
+var backType = [
+  back1:{name:"deadlift"},
+  back2:{name:"pulldown"},
+  back3:{name:"seated-row"},
+  back4:{name:"bent-over-row"},
+  back5:{name:"single-dumbell-row"}
+]
+
+var armsType = [
+  arms1:{name:"dumbell-curl"},
+  arms2:{name:"hammer-curl"},
+  arms3:{name:"seated-hammer-curl"},
+  arms4:{name:"seated-dumbell-curl"},
+  arms5:{name:"barbell-curl"},
+  arms6:{name:"cable-curl"},
+  arms7:{name:"tricep-extension"},
+  arms8:{name:"lying-tricep-extension"},
+  arms9:{name:"cable-tricep-extension"}
+]
+
+var chestType = [
+  chest1:{name:"chest-press"},
+  chest2:{name:"dumbell-chest-press"},
+  chest3:{name:"incline-chest-press"},
+  chest4:{name:"dumbell-incline-chest-press"},
+  chest5:{name:"decline-press"},
+  chest6:{name:"dumbell-decline-chest-press"},
+  chest7:{name:"cable-fly"},
+  chest8:{name:"cable-incline-fly"},
+  chest9:{name:"cable-decline-fly"}
+]
+
+var shouldersType = [
+  shoulders1:{name:"dumbell-shoulder-press"},
+  shoulders2:{name:"barbell-shoulder-press"},
+  shoulders3:{name:"dumbell-shoulder-fly"},
+  shoulders4:{name:"face-pull"},
+  shoulders5:{name:"armold-press"}
+]
